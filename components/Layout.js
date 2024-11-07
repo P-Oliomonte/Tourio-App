302f8ea42e166f0c5c9f5c2949e6f8babdbe0fd9
Phillip Oehlenberg <hi@phillip-oehlenberg.com>
2024-11-07 13:38:37 +0100

    Add max-width

--- components/Layout.js
+++ components/Layout.js
@@ -3,12 +3,14 @@ import styled from "styled-components";
 import Head from "next/head.js";
 
 const Main = styled.main`
+  margin: 0 auto;
   display: grid;
   gap: 0.5rem;
   margin-top: 5rem;
   padding: 0.5rem;
   position: relative;
   width: 100%;
+  max-width: 800px;
 `;
 
 export default function Layout({ children }) {
